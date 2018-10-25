08c4c9a4b0a31a6105ee5b11a9167beeb2387818
贝塔(邓乐) <le.deng@fanli.com>
2018-10-25 21:36:46 +0800

    init proj

---
+++ CarthageTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageTestApp
+//
+//  Created by 邓乐 on 2018/10/25.
+//  Copyright © 2018 dl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
